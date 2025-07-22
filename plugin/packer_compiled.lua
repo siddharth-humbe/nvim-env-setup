975c94157ab67d3171649b75ef1efbe2120b1b3b
siddharth humbe <h.siddharth70@gmail.com>
2025-07-23 00:18:27 +0530

    added the nvim tree to the codebase helping naviate

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/Users/siddharth/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
   },
+  ["nvim-tree.lua"] = {
+    loaded = true,
+    path = "/Users/siddharth/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    url = "https://github.com/nvim-tree/nvim-tree.lua"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/Users/siddharth/.local/share/nvim/site/pack/packer/start/packer.nvim",
